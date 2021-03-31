1fb257671cbe14beb747413508c0f385b940658a
ffadelm <faysalfadelm0107@gmail.com>
2021-04-01 05:29:56 +0700

    Menu and Floating Button

--- app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
+++ app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.sampleconstraintlayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -82,5 +85,23 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //method untuk menampilkan menu
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //membuat kondisi jika memilih mnDaftar
+        if(item.getItemId() == R.id.mnDaftar){
+            //memanggil ACT pendaftaran
+            Intent i = new Intent(getApplicationContext(), Pendaftaran.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
